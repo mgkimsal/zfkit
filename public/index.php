a4d8a6871229a999a4beacce42f8c95a416f3d0b
Michael Kimsal <Michael Kimsal>
2010-09-05 10:42:45 -0400

    putting timezone declaration in to appliation.ini - taken from bryanz @ http://github.com/bzarzuela/zfkit/commit/438d0cf271ce7186e1d026133fe7c6862e8c81ca

--- public/index.php
+++ public/index.php
@@ -1,8 +1,4 @@
 <?php
-// might need to set the time zone if your php doesn't have it set
-// date_default_timezone_set('America/New_York');
-
-
 include("../base.php");
 $application->bootstrap()
             ->run();

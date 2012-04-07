a10b9462c65b828e1e9d547006261ea01148ce31
Michael Kimsal <michael@kimsal.com>
2012-04-07 15:58:10 -0400

    misc login/reg processing info

--- application/controllers/AuthController.php
+++ application/controllers/AuthController.php
@@ -9,7 +9,8 @@ class AuthController extends Zend_Controller_Action
 	public function init()
 	{
 		$this->view->pageTitle = "Log in";
-		$this->view->pageSubTitle = "";		
+		$this->view->pageSubTitle = "";
+        $this->_config = Zend_Registry::get("config");
 	}
 	
     public function indexAction()
@@ -21,6 +22,7 @@ class AuthController extends Zend_Controller_Action
     	}
         $form = new Form_Login();
         $request = $this->getRequest();
+        $errors = array();
         if ($request->isPost()) {
             if ($form->isValid($request->getPost())) {
             	if ($this->_process($_POST)) {
@@ -28,8 +30,10 @@ class AuthController extends Zend_Controller_Action
                     $this->_helper->redirector('index', 'index');
                 }
             }
+            $errors[] = "We couldn't log you in with that information";
         }
         $this->view->form = $form;
+        $this->view->errors = $errors;
     }
 
     public function logoutAction()
@@ -40,12 +44,64 @@ class AuthController extends Zend_Controller_Action
 
     public function forgotAction()
     {
+        $this->view->pageTitle = "I forgot my password";
+        if($_POST) {
+
+            $u = R::findOne("account", " username=? or email=?", array($_POST['username'], $_POST['email']));
+            if($u==null)
+            {
+                $errors[] = "We couldn't find an account with that information";
+                $this->view->errors = $errors;
+            } else {
+                $u->resetkey = sha1(microtime()).":".time();
+                R::store($u);
+                $link = $this->_config['base']['fullurl']."auth/forgotprocess/key/".$u->resetkey;
+                $email = file_get_contents(APPLICATION_PATH."/configs/".$this->_config['register']['forgotPasswordEmail']);
+                $email = str_replace("[url]", $link, $email);
+
+                $zm = new Zend_Mail();
+                $zm->setSubject("Reset your password");
+                $zm->addTo($u->email);
+                $zm->setBodyText($email);
+                $zm->send();
+                $info[] = "We've sent a link to your email - please check that email to reset your password";
+                $this->view->info = $info;
+
+            }
+
+
+        }
 
     }
 
     public function forgotprocessAction()
     {
-
+        $this->view->pageTitle = "Reset my password";
+        $key = $this->_getParam("key");
+        $u = R::findOne("account", " resetkey=?", array($key));
+        $this->view->user = $u;
+        $errors = array();
+        $info = array();
+        if($_POST)
+        {
+            if(trim($_POST['password'])=='')
+            {
+                $errors[] = "Can't have a blank password";
+            }
+            if($_POST['password']!=$_POST['password2'])
+            {
+                $errors[] = "Passwords don't match";
+            }
+            if(count($errors)==0)
+            {
+                $u->password = sha1($_POST['password']);
+                R::store($u);
+                $info[] = "Password successfully updated";
+                $this->view->done = true;
+            }
+            $this->view->errors = $errors;
+            $this->view->info = $info;
+        }
     }
 
     protected function _process($values)

14c46ebb28c0c505a96b06d95d110dfab02d6447
Michael Kimsal <mgkimsal@gmail.com>
2013-05-29 00:21:11 -0400

    frontpage and footer fix

--- application/views/helpers/Footer.php
+++ application/views/helpers/Footer.php
@@ -4,15 +4,19 @@ class Zend_View_Helper_footer extends Zend_View_Helper_Abstract
 {
     public function footer()
     {
+		$config = Zend_Registry::get("config");
+		$phone = $config['contact']['phone']?:"";
+		$email = $config['contact']['email']?:"";
+		$copyright = $config['contact']['copyright']?:"";
 $s = <<<EOD
 <footer>
     <div class="container footer">
 <h3>Contact</h3>
 
-        <p>Phone: 919-648-0477</p>
-        <p>Email: <a href="mailto:info@fieldflag.com">info@fieldflag.com</a></p>
+        <p>Phone: $phone</p>
+        <p>Email: <a href="mailto:$email">$email</a></p>
         <br/>
-<p>&copy; 2012 fieldflag.com </p>
+<p>$copyright </p>
     </div>
 
 </footer>
